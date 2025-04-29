82da4c4228cbbc6a5e2e0c3f992842034896d10c
hericlessoares <84105324+hericlessoares@users.noreply.github.com>
2025-04-29 20:10:59 -0300

    atualização
    v2

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,22 @@
 import { HeaderContainer,HeaderContent,NewTransactionButton } from "./styles";
-
+import * as Dialog from '@radix-ui/react-dialog';
 import logoImg from '../../assets/logo.svg';
+
+import { NewTransacionModal } from "../NewTransacionModal";
 
 export function Header(){
     return(
         <HeaderContainer>
             <HeaderContent>
                 <img src={logoImg}alt="" />
-                <NewTransactionButton>Nova transação</NewTransactionButton>
+                <Dialog.Root>
+                  <Dialog.Trigger asChild>
+                    <NewTransactionButton>Nova transação</NewTransactionButton>
+                  </Dialog.Trigger>
+                
+                <NewTransacionModal/>
+
+                </Dialog.Root>
             </HeaderContent>
         </HeaderContainer>
               
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,4 +1,5 @@
 import { Header } from "../../components/Header"
+import { SearchForm } from "../../components/SearchForm"
 import { Summary } from "../../components/Summary"
 
 import { TransactionsContainer,TransactionsTable,PriceHighlight } from "./styles"
@@ -9,6 +10,7 @@ export function Transactions(){
             <Summary/>
 
             <TransactionsContainer>
+            <SearchForm/>
                 <TransactionsTable>
                     <tbody>
                         <tr>
